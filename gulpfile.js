2c69b240fa610e6dcb0d5696285f6b962b440de6
Varis Darasirikul <varis.dara@gmail.com>
2018-07-18 23:31:43 +0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,17 @@ var jshint = require('gulp-jshint');
 var imagemin = require('gulp-imagemin');
 var connect = require('connect');
 var serve = require('serve-static');
+var browsersync = require('browser-sync');
+var postcss = require('gulp-postcss'); // Added
+var cssnext = require('postcss-cssnext'); // Added
+var cssnano = require('cssnano'); // Added
+var browserify = require('browserify'); // Added
+var source = require('vinyl-source-stream'); // Added
+var buffer = require('vinyl-buffer'); // Added
+var babelify = require('babelify'); // Added
 
-gulp.task('styles', function () {
+// For myth
+gulp.task('myth-styles', function () {
     return gulp.src('app/css/*.css')
         .pipe(concat('all.css'))
         .pipe(myth())
@@ -22,6 +31,17 @@ gulp.task('sass-styles', function () {
         .pipe(gulp.dest('dist'));
 });
 
+// Styles Task
+gulp.task('styles', function () {
+    return gulp.src('app/css/*.css')
+        .pipe(concat('all.css'))
+        .pipe(postcss([
+            cssnext(),
+            cssnano()
+        ]))
+        .pipe(gulp.dest('dist'));
+});
+
 gulp.task('scripts', function () {
    return gulp.src('app/js/*.js')
        .pipe(jshint())
@@ -31,6 +51,15 @@ gulp.task('scripts', function () {
        .pipe(gulp.dest('dist'));
 });
 
+// Browserify Task
+gulp.task('browserify', function() {
+    return browserify('./app/js/app.js')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('dist'));
+});
+
 gulp.task('images', function () {
    return gulp.src('app/img/*')
        .pipe(imagemin())
@@ -38,9 +67,9 @@ gulp.task('images', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch('app/css/*.css', ['styles']);
-    gulp.watch('app/js/*.js', ['scripts']);
-    gulp.watch('app/img/*', ['images']);
+    gulp.watch('app/css/*.css', ['styles', browsersync.reload]);
+    gulp.watch('app/js/*.js', ['scripts', browsersync.reload]);
+    gulp.watch('app/img/*', ['images', browsersync.reload]);
 });
 
 gulp.task('server', function () {
@@ -51,4 +80,12 @@ gulp.task('server', function () {
        });
 });
 
-gulp.task('default', ['styles', 'scripts', 'images', 'watch']);
+gulp.task('browsersync', function () {
+   return browsersync({
+      server: {
+          baseDir: './'
+      }
+   });
+});
+
+gulp.task('default', ['styles', 'scripts', 'images', 'browsersync', 'watch']);
